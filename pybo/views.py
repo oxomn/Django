25891f4cbcf0a319f9b39985ecc881667f905dac
yeain <oxov99@gmail.com>
2021-06-10 15:42:58 +0900

    3-02 page_number

--- pybo/views.py
+++ pybo/views.py
@@ -8,14 +8,22 @@ from django.http import HttpResponse
 from .models import Question
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
-
+from django.core.paginator import Paginator
 #----------------------------------#
 def index(request):
     """
     pybo 목록 출력
     """
+
+     # 입력 파라미터
+    page = request.GET.get('page', '1')  # 페이지
+
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list' : question_list}
+    
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    
+    context = {'question_list' : page_obj}
 
     return render(request, 'pybo/question_list.html', context)
     
