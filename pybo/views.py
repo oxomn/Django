888536791a13f78e009f4a07a3bbd0ee43d0c6e6
yeain <oxov99@gmail.com>
2021-06-10 10:11:23 +0900

    a

--- pybo/views.py
+++ pybo/views.py
@@ -7,6 +7,7 @@ from django.http import HttpResponse
 #------------------------------------------#
 from .models import Question
 from django.utils import timezone
+from .forms import QuestionForm
 #----------------------------------#
 def index(request):
     """
@@ -37,3 +38,11 @@ def answer_create(request, question_id):
     question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
 
     return redirect('pybo:detail', question_id=question.id)
+
+#----------------------------------------------#
+def question_create(request):
+    """
+    pybo 질문등록
+    """
+    form = QuestionForm()
+    return render(request, 'pybo/question_form.html', {'form': form})
