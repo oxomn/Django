c31c40e182d50e6f645e7d781d971641261da230
yeain <oxov99@gmail.com>
2021-06-11 10:28:46 +0900

    sign up 3-06

--- common/views.py
+++ common/views.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.shortcuts import render
-
+#-----------------------#
+from django.shortcuts import render, redirect
+from common.forms import UserForm
+from django.contrib.auth import authenticate, login
 # Create your views here.
+def signup(request):
+    """
+    계정생성
+    """
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserForm()
+    return render(request, 'common/signup.html', {'form': form})
