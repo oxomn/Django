b05ca8fb564454bb5dae540493c5c572e8f33d36
yeain <oxov99@gmail.com>
2021-06-11 15:54:18 +0900

    views.py 파일 분리

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -5,8 +5,29 @@ from ..models import Question
 
 
 def index(request):
-    (... 생략 ...)
+    """
+    pybo 목록 출력
+    """
+
+     # 입력 파라미터
+    page = request.GET.get('page', '1')  # 페이지
+
+    question_list = Question.objects.order_by('-create_date')
+    
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    
+    context = {'question_list' : page_obj}
+
+    return render(request, 'pybo/question_list.html', context)
 
 
 def detail(request, question_id):
-    (... 생략 ...)
+    """
+    pybo 내용 출력
+    """
+    
+    question = get_object_or_404(Question, pk=question_id)
+
+    context = {'question':question}
+    return render(request, 'pybo/question_detail.html', context)
